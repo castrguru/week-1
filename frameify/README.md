dcfd6b65af62a8f558904112157b41e88c201872
Shomari <nyusternie@sdot.io>
2025-01-16 18:53:15 -0500

    Add source.

---
+++ frameify/README.md
@@ -0,0 +1,5 @@
+# Frameify by Castr GÜRŲ
+
+### https://frameify.xyz
+
+Frameify any website into a Farcaster v2 frame.
---
+++ frameify/src/consts.ts
@@ -0,0 +1,3 @@
+/* Define global constants. */
+export const SITE_TITLE = `Frameify by Castr GÜRŲ`
+export const SITE_DESCRIPTION = `Frameify any website into a Farcaster v2 frame.`
